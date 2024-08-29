922e89d04b840e968f9a00d1581feaa1cf59f0c0
chepeniv <jose.olmos@atlasschool.com>
2024-08-29 03:42:20 -0500

    [untested] conformed to negative case handling

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -2,7 +2,7 @@
 
 def print_last_digit(number):
     if number < 0:
-        last = -(-number % 10)
+        last = -number % 10
     else:
         last = number % 10
     print("{}".format(last), end='')
