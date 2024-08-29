aa086a17485cd151ae59343fd1d2b3915c09ac81
chepeniv <jose.olmos@atlasschool.com>
2024-08-29 04:06:34 -0500

    [style] removed whitespace

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -6,7 +6,7 @@ def fizzbuzz():
             print("Fizz", end='')
         if i % 5 == 0:
             print("Buzz", end='')
-        if i % 5 != 0 and i % 3 != 0 :
+        if i % 5 != 0 and i % 3 != 0:
             print("{}".format(i), end=' ')
         else:
             print(" ", end='')
