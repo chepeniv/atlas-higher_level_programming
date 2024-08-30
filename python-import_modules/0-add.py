857b9dea2342710577f4147539dfcdc4f054b878
chepeniv <jose.olmos@atlasschool.com>
2024-08-29 19:07:54 -0500

    [passing] misread instructions, new approach

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-math = __import__('add_0')
+from add_0 import add
+
 a = 1
 b = 2
-print("{} + {} = {}".format(a, b, math.add(a, b)))
+print("{} + {} = {}".format(a, b, add(a, b)))
