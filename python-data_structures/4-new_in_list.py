437989ae2b3ed5352892f3e48028c9798d736d65
chepeniv <jose.olmos@atlasschool.com>
2024-08-29 22:05:36 -0500

    [passing] p4-t4

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def new_in_list(old_list, idx, value):
+    new_list = old_list.copy()
+    if idx > 0 and idx < len(old_list) - 1:
+        new_list[idx] = value
+    return new_list
