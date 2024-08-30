c365caa21921b89358aaaee5b2017c20c394a5da
chepeniv <jose.olmos@atlasschool.com>
2024-08-29 22:07:53 -0500

    [passing] corrected conditional range-check

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,6 +2,6 @@
 
 def new_in_list(old_list, idx, value):
     new_list = old_list.copy()
-    if idx > 0 and idx < len(old_list) - 1:
+    if idx >= 0 and idx < len(old_list):
         new_list[idx] = value
     return new_list
