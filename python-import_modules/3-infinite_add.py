65bfe1505cfd9a4190d59d4f02ace3a604c805c5
chepeniv <jose.olmos@atlasschool.com>
2024-08-29 20:48:11 -0500

    [untested] correct empty-case output

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
     from sys import argv
 
@@ -9,4 +8,4 @@ total = 0
 if length > 0:
     for i in range(1, length + 1):
         total += int(argv[i])
-    print("{}".format(total))
+print("{}".format(total))
