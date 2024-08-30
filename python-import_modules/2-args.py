2893c92d04f958af1605ffedbc2c1802083fe6b3
chepeniv <jose.olmos@atlasschool.com>
2024-08-29 20:38:57 -0500

    [stlyle] removed trailing whitespace

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,13 +5,12 @@ if __name__ == "__main__":
 
 length = len(argv) - 1
 
-if length == 0: 
+if length == 0:
     print("0 arguments.")
-elif length == 1: 
+elif length == 1:
     print("1 argument:")
-else: 
+else:
     print("{} arguments:".format(length))
 
 for i in range(1, length + 1):
     print("{}: {}".format(i, argv[i]))
-
