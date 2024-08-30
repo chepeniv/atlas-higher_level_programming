ccbec65f75ebb50b82fe8b2b336452cabc9b98fa
chepeniv <jose.olmos@atlasschool.com>
2024-08-30 01:39:58 -0500

    [untested] p4-t10 blind attempt

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def divisible_by_2(ints=[]):
+    log = []
+
+    for i in range(0, len(ints))
+        if ints[i] % 2 == 0:
+            log.append(True)
+        else:
+            log.append(False)
+    return log
