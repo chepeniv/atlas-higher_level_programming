69f743f7dab4391554a4009a4c35a3b200022f30
chepeniv <jose.olmos@atlasschool.com>
2024-08-30 01:29:07 -0500

    [untested] p4-t9 blind attempt

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def max_integer(nums=[]):
+    if len(nums) == 0:
+        return None
+
+    M = nums[0]
+    for i in nums:
+        if i > M:
+            M = i
+    return M
+
+
