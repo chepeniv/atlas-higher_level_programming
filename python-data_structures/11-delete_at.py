e65a7a51decc0691a8198d8c99b1352bf5b02dea
chepeniv <jose.olmos@atlasschool.com>
2024-08-30 02:00:04 -0500

    [untested] handled negative-index case

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def delete_at(items=[], i=0):
-    if len(items) > 0:
+    if len(items) > 0 and i >= 0:
         first = items[:i]
         second = items[i+1:]
         items.clear()
