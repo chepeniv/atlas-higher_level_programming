1b85bb4138bcf457ff7f0ff7c7dc8a9efe27d3c7
chepeniv <jose.olmos@atlasschool.com>
2024-08-29 20:37:28 -0500

    [passing] preventing run on import

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-import sys
 
-length = len(sys.argv) - 1
+if __name__ == "__main__":
+    from sys import argv
+
+length = len(argv) - 1
 
 if length == 0: 
     print("0 arguments.")
@@ -11,5 +13,5 @@ else:
     print("{} arguments:".format(length))
 
 for i in range(1, length + 1):
-    print("{}: {}".format(i, sys.argv[i]))
+    print("{}: {}".format(i, argv[i]))
 
