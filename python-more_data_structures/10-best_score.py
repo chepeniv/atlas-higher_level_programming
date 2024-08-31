1420266aa1b194b1616b13965c4b438e9db250d4
chepeniv <jose.olmos@atlasschool.com>
2024-08-30 23:18:40 -0500

    [untested] p5-10

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def best_score(D):
+    top = 0
+    winner = None
+    for k, v in E.items():
+        if v > top:
+            top = v
+            winner = k
+    return winner
