40f3ce56da97661febd4a5918e810a019fcf0899
chepeniv <jose.olmos@atlasschool.com>
2024-08-31 01:04:38 -0500

    [passing] parenthesized multiple exceptions

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -4,5 +4,5 @@ def safe_print_integer(v):
     try:
         print("{:d}".format(v))
         return True
-    except TypeError, ValueError:
+    except (TypeError, ValueError):
         return False
