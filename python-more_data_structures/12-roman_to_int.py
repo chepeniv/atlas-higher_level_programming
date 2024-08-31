5e5670bd63b741a984eb29e6890ba373d367c02e
chepeniv <jose.olmos@atlasschool.com>
2024-08-31 07:54:16 -0500

    [style] removed trailing whitespace, broke long line apart

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
 def roman_to_int(roman):
     calc = 0
-    ref = {'Null': 0, 'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    ref = {
+        'Null': 0,
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
 
     if type(roman) is not str:
         return 0
@@ -9,14 +18,14 @@ def roman_to_int(roman):
     for i in range(len(roman)):
         a = roman[i]
 
-        try: 
+        try:
             b = roman[i + 1]
         except (IndexError):
             b = 'Null'
 
         m = ref.get(a)
         n = ref.get(b)
-        if m >= n: 
+        if m >= n:
             calc += m
         else:
             calc -= m
