6174bb85833686a881e37ddf6717ecd877d24828
chepeniv <jose.olmos@atlasschool.com>
2024-08-31 07:50:40 -0500

    [untested] added type check for

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,6 +3,9 @@ def roman_to_int(roman):
     calc = 0
     ref = {'Null': 0, 'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 
+    if type(roman) in not str:
+        return 0
+
     for i in range(len(roman)):
         a = roman[i]
 
