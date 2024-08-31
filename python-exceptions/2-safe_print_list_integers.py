b74f355269f4ec4d97bef6e44fcf045772672ef5
chepeniv <jose.olmos@atlasschool.com>
2024-08-31 01:21:27 -0500

    [passing] p6-t2

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(L=[], x=0):
+    total = 0
+
+    for i in range(0, x):
+        try:
+            print("{:d}".format(L[i]), end='')
+            total += 1
+        except (TypeError, ValueError):
+            continue
+        except (IndexError):
+            break
+    print("")
+    return total
