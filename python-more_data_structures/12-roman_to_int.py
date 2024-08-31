5fe3c56828218c3fd9fcac5819286f0c661adb0b
chepeniv <jose.olmos@atlasschool.com>
2024-08-31 00:31:47 -0500

    [save] p5-t12

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def roman_to_int(roman):
+    ref = "IVXLCDM" # 1, 5, 10, 50, 100, 500, 1000
+    if roman is not str:
+        return 0
+    else:
+    return list(map(lambda i: i * n, L))
