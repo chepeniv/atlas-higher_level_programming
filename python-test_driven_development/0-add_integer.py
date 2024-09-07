3d2e098ef7339f0b04df3b050144e0c0b7f3527b
chepeniv <jose.olmos@atlasschool.com>
2024-09-07 16:38:06 -0500

    [style] conformed to type check conventions

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -6,13 +6,13 @@
 def add_integer(a, b=98):
     """adds two given intgers together
     """
-    if type(a) == float:
+    if type(a) is float:
         a = int(a)
-    if type(b) == float:
+    if type(b) is float:
         b = int(b)
-    if type(a) != int:
+    if type(a) is not int:
         raise TypeError("a must be an integer")
-    if type(b) != int:
+    if type(b) is not int:
         raise TypeError("b must be an integer")
     if abs(a) > 1000 or abs(b) > 1000:
         raise OverflowError("numbers exceed range of -1000 to 1000")
