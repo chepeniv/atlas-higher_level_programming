c9de64f1a3ef5ec887677d1e04e20434e1f3aa08
chepeniv <jose.olmos@atlasschool.com>
2024-09-07 04:39:33 -0500

    [untested] init submission

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -6,7 +6,12 @@
 class Square:
     """defines square objects with only one parameter
     """
-    def __init__(self, size):
+    def __init__(self, size=0):
         """constructs a square with side of length `size`
         """
-        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must >= 0")
+        else:
+            self.__size = size
