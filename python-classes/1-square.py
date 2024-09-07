d83eff2e3511a7d1332f444bec155235ef4f65f9
chepeniv <jose.olmos@atlasschool.com>
2024-09-07 03:28:58 -0500

    [doc] added descriptions to class square

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
 
 class square:
+    """defines square objects with only one parameter
+
+    Attributes:
+        size(int): denotes the length of any side of this square
+    """
     def __init__(self, size):
-       self.__size = size
+        """creates a square object with only a single parameter
+
+        Args:
+            size(int): length of square's side
+        """
+        self.__size = size
