98d601e665195c8630202998add86182b8ac8e2c
chepeniv <jose.olmos@atlasschool.com>
2024-09-07 13:21:34 -0500

    [untested] initial submission

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,6 +3,17 @@
 """
 
 
-def add_integer(a, b=98)
+def add_integer(a, b=98):
     """adds two given intgers together
+
+    testing considerations - checking type, testing range
     """
+    if type(a) == float:
+        a = int(a)
+    if type(b) == float:
+        b = int(b)
+    if type(a) != int:
+        raise TypeError("a must be an integer")
+    if type(b) != int:
+        raise TypeError("b must be an integer")
+    return a + b
