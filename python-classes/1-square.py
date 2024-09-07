285e59979a1070882222a28fb02811c699e5c30f
chepeniv <jose.olmos@atlasschool.com>
2024-09-07 04:41:37 -0500

    [file] renamed files

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -6,12 +6,7 @@
 class Square:
     """defines square objects with only one parameter
     """
-    def __init__(self, size=0):
+    def __init__(self, size):
         """constructs a square with side of length `size`
         """
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must >= 0")
-        else:
-            self.__size = size
+        self.__size = size
---
+++ python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""this module carries a single geometric class
+"""
+
+
+class Square:
+    """defines square objects with only one parameter
+    """
+    def __init__(self, size=0):
+        """constructs a square with side of length `size`
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must >= 0")
+        else:
+            self.__size = size
