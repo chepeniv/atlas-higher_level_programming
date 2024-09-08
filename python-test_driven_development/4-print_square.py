c2b1f16f7b9086f70cf3da816daf7a6f63389489
chepeniv <jose.olmos@atlasschool.com>
2024-09-08 14:19:27 -0500

    [untested] core code created

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""module with a single function to use doctest on
+"""
+
+
+def print_square(size):
+    """output a square made out of # based on size
+    """
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+    for row in range(size):
+        print("{}".format('#' * size))
