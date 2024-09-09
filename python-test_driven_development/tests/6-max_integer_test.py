f6aa16e97ccbea9a5beb6b2ca97f51b0804e5df9
chepeniv <jose.olmos@atlasschool.com>
2024-09-09 04:45:19 -0500

    [unittest] added more test

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -9,11 +9,20 @@ max_int = __import__('6-max_integer').max_integer
 class TestMaxIntFunc(unittest.TestCase):
     """class containing all testing functions for max_integer
     """
-    def test_expected_use(self):
+    def test_max_at_end(self):
         self.assertEqual(max_int([0, 1, 2]), 2)
+    def test_max_at_start(self):
         self.assertEqual(max_int([2, 1, 0]), 2)
-        self.assertEqual(max_int([-1, 0, 1]), 1)
+    def test_max_at_middle(self):
+        self.assertEqual(max_int([2, 4, 1]), 4)
+    def test_one_negative(self):
         self.assertEqual(max_int([1, 0, -1]), 1)
+    def test_only_negative(self):
+        self.assertEqual(max_int([-1, -6, -12]), -1)
+    def test_single_element(self):
+        self.assertEqual(max_int([3]), 3)
+    def test_empty(self):
+        self.assertEqual(max_int([]), None)
 
     if __name__== '__main__':
         unittest.main()
