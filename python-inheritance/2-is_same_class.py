7b3dca407ed2a08f59dd011bfe5a9931362dddb4
chepeniv <jose.olmos@atlasschool.com>
2024-09-12 17:42:59 -0500

    [passing] init submission

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""defines a single function
+"""
+
+
+def is_same_class(obj, a_class):
+    """test whether or not the given obj is exactly and instance of a_class
+    """
+    if type(obj) == a_class:
+        return True
+    else:
+        return False
