1e0cca7b61460bbfa39417f7099301eb14a3df0c
chepeniv <jose.olmos@atlasschool.com>
2024-09-12 17:54:03 -0500

    [untested] init submission

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""defines a single function
+"""
+
+
+def is_kind_of_class(obj, kind):
+    """test whether or not the given obj is an instance of given class kind
+    """
+    return issubclass(obj, kind)
