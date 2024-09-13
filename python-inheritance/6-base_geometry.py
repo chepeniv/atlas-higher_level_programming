57b6b3fb7a2696fbf67a1e64fe5502352e48a92a
chepeniv <jose.olmos@atlasschool.com>
2024-09-12 20:44:57 -0500

    [untested] added an unimplemented method

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""defines a single class
+"""
+
+
+class BaseGeometry():
+    """an empty class
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
