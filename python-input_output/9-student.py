aecc93b9c756d9114651c56836347621d5670adb
chepeniv <jose.olmos@atlasschool.com>
2024-09-13 18:11:58 -0500

    [fix] dictionary: commas to colons

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -13,7 +13,7 @@ class Student:
 
     def to_json(self):
         json_dict = {}
-        json_dict.update({"first_name", self.first_name})
-        json_dict.update({"last_name", self.last_name})
-        json_dict.update({"age", self.age})
+        json_dict.update({"first_name": self.first_name})
+        json_dict.update({"last_name": self.last_name})
+        json_dict.update({"age": self.age})
         return json_dict
