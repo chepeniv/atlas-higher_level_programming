59e5c7a0d23e96e03319763e5cf7ec3235b243ef
chepeniv <jose.olmos@atlasschool.com>
2024-09-13 11:59:27 -0500

    [passing] created function that writes to a file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""defines a single function for file i/o
+"""
+
+
+def write_file(filename="", text=""):
+    """writes plaintext to a given utf-8 textfile
+    """
+    with open(filename, mode="w") as file:
+        file.write(text)
