fb3e5a108902111a6b7f99f98b601dbfa70a936a
chepeniv <jose.olmos@atlasschool.com>
2024-09-12 20:31:53 -0500

    [untested] clarified task goal; adjusted logic accordingly

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -6,4 +6,4 @@
 def inherits_from(obj, kind):
     """test whether or not the given obj is an instance of given class kind
     """
-    return issubclass(type(obj), kind)
+    return issubclass(type(obj), kind) and not isinstance(obj, kind)
