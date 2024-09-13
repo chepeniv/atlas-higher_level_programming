44b9b7cf003ff9588509ff3f96e1eb6c35b302a7
chepeniv <jose.olmos@atlasschool.com>
2024-09-12 21:34:28 -0500

    [untested] included the base class in the script

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
-"""defines a single class
+"""defines a series of classes using inheritance
 """
 
+
+class BaseGeometry():
+    """the base class
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
 
 class Rectangle(BaseGeometry):
     """inherits and extends parent class
