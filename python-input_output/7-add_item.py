9f99acef3e54474a2fbe8303d4b11fc14027edd8
chepeniv <jose.olmos@atlasschool.com>
2024-09-13 14:30:25 -0500

    [passing] created script that reads and writes to json file

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""defines a single function for json file inter-operability
+"""
+
+import sys
+import os
+import json
+
+
+load_json = __import__('6-load_from_json_file').load_from_json_file
+save_json = __import__('5-save_to_json_file').save_to_json_file
+
+try:
+    jdata = load_json('add_item.json')
+except:
+    jdata =[]
+
+
+for item in range(1, len(sys.argv)):
+    jdata += [sys.argv[item]]
+
+save_json(jdata, 'add_item.json')
