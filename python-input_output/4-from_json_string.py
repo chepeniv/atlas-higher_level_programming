5ef196c9b48948b9ea669038ab98680000419687
chepeniv <jose.olmos@atlasschool.com>
2024-09-13 12:20:34 -0500

    [untested] created function that returns a python object defined by a json string

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""defines a single function for json file inter-operability
+"""
+
+import json
+
+
+def from_json_string(jstring):
+    """returns a python object defined by the given json string representation
+    ""
+    return json.loads(jstring)
