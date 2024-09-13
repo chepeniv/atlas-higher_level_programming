7602351ce5887a66a0f36a8bde60d0a55934bb1c
chepeniv <jose.olmos@atlasschool.com>
2024-09-12 20:53:33 -0500

    [untested] added integer_validator instance method

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""defines a single class
+"""
+
+
+class BaseGeometry():
+    """an empty class
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        else if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
