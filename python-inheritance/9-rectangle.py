111b3e001edbb0e49b47b5b838683836485e6e76
chepeniv <jose.olmos@atlasschool.com>
2024-09-12 21:47:51 -0500

    [untested] reimplemented Rectangle with area() defined and custom__str__

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""defines a series of classes using inheritance
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """inherits and extends parent class
+    """
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
