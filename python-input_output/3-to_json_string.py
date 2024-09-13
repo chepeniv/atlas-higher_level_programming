0a8132858654d00ed40c8c08c0448e38b614d778
chepeniv <jose.olmos@atlasschool.com>
2024-09-13 12:15:16 -0500

    [passing] created function that converts python obj to json representation

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""defines a single function for json file inter-operability
+"""
+
+import json
+
+def to_json_string(obj):
+    """returns the json representation of the given object
+    """
+    return json.dumps(obj)
