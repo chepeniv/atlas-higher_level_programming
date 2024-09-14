89581e93aa30b4072de55f9f99d7d8fce6eb518c
chepeniv <jose.olmos@atlasschool.com>
2024-09-13 19:56:59 -0500

    [file] corrected the right file this time

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -19,13 +19,3 @@ def pascal_triangle(n):
             new.append(1)
             triangle.append(new)
     return triangle
-    
-def print_triangle(triangle):
-    """
-    Print the triangle
-    """
-    for row in triangle:
-        print("[{}]".format(",".join([str(x) for x in row])))
-
-
-print_triangle(pascal_triangle(5))
