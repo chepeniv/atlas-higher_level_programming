33cdb0552314ec93dfb4140589b8555545e09262
chepeniv <jose.olmos@atlasschool.com>
2024-09-19 04:50:48 -0500

    [untested] trying from-style import

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -3,20 +3,7 @@
 """
 
 
-class Base:
-    """ a simple class that currently only handles id assignments
-    """
-    __nb_objects = 0
-
-    def __init__(self, id=None):
-        """ initializer for Base
-        """
-        if id is not None:
-            self.id = id
-        else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
-
+from base import Base
 
 class Rectangle(Base):
     """ foundational definition for Rectangle class
