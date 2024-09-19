507a0a9a4916f29ac4c72a3bc402f279bdd81e03
chepeniv <jose.olmos@atlasschool.com>
2024-09-19 04:39:20 -0500

    [untested] created Rectangle(Base) class

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+""" barebones module holding a single class
+"""
+
+
+Base = __import__("base").Base
+
+
+class Rectangle(Base):
+    """ foundational definition for Rectangle class
+    """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """ initializer for Base
+        """
+        super().__init__(id)
+
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @property
+    def x(self):
+        return self.__x
+
+    @property
+    def y(self):
+        return self.__y
+
+    @width.setter
+    def width(self, width):
+        self.__width = width
+
+    @height.setter
+    def height(self, height):
+        self.__height = height
+
+    @x.setter
+    def x(self, x):
+        self.__x = x
+
+    @y.setter
+    def y(self, y):
+        self.__y = y
