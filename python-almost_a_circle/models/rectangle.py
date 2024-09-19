a3677374a0b0e7b7e9922595610f02ceef7f4e94
chepeniv <jose.olmos@atlasschool.com>
2024-09-19 04:46:15 -0500

    [untested] included Base class def directly into module

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -3,7 +3,19 @@
 """
 
 
-import base
+class Base:
+    """ a simple class that currently only handles id assignments
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ initializer for Base
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
 
 
 class Rectangle(base.Base):
