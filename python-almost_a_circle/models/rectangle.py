24306f21855eaa55b853ae3159c800cbfa3b2095
chepeniv <jose.olmos@atlasschool.com>
2024-09-19 04:58:10 -0500

    [git] restored to previous state

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -3,7 +3,20 @@
 """
 
 
-Base = __import__('base').Base
+class Base:
+    """ a simple class that currently only handles id assignments
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ initializer for Base
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
 
 class Rectangle(Base):
     """ foundational definition for Rectangle class
