eaf25d8d4d7c118d6ae316c393b8238091febe70
chepeniv <jose.olmos@atlasschool.com>
2024-09-19 03:34:29 -0500

    [untested] documented

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
+""" barebones module holding a single class
+"""
 
 class Base:
+    """ a simple class that currently only handles id assignments
+    """
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """ initializer for Base
+        """
         if id is not None:
             self.id = id
         else:
