9dffb5cfeb529eef792c4accf207880264def6fa
chepeniv <jose.olmos@atlasschool.com>
2024-09-19 23:33:06 -0500

    [untested] edited the values of update()'s internal list

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -32,9 +32,8 @@ class Square(Rectangle):
         """ modifies the values of the current object based either on argument
         position or keywords
         """
-        keys = ['id', '_Rectangle__width', '_Rectangle__height',
-                '_Rectangle__x', '_Rectangle__y']
-        argkeys = ['id', 'size', 'size', 'x', 'y']
+        keys = ['id', '_Rectangle__width', '_Rectangle__x', '_Rectangle__y']
+        argkeys = ['id', 'size', 'x', 'y']
         if len(args) > 0:
             for item in range(len(args)):
                 self.__dict__.update({keys[item]: args[item]})
