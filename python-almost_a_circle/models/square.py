5198aa64ba47b86580489dc77c467b2948c3259d
chepeniv <jose.olmos@atlasschool.com>
2024-09-19 22:59:42 -0500

    [untested] created the Square(Rectangle) class

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" barebones module holding a single class
+"""
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """ foundational definition for Rectangle class
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """ initializer for Rectangle
+        """
+        super().__init__(id, size, size, x, y)
+
+    def __str__(self):
+        obj_str = "[Square] ({}) {}/{} - {}"
+        return obj_str.format(self.id, self.x, self.y, self.width)
