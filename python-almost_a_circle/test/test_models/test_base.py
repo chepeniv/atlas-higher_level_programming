365d7de89de1a7bbf5c4205b02f72993f11dff77
chepeniv <jose.olmos@atlasschool.com>
2024-09-23 04:51:28 -0500

    [unittest] tinkered around too much with paths

--- python-almost_a_circle/test/test_models/test_base.py
+++ python-almost_a_circle/test/test_models/test_base.py
@@ -1,50 +1,55 @@
 #!/usr/bin/python3
-"""python test script for Base class
+""" python test script for Base class
     execute test :
         python3 -m unittest test.test_models.test_base
+
+    reference list of assert methods: 
+
+    assertEqual(a, b)           a == b
+    assertNotEqual(a, b)        a != b
+    assertTrue(x)               bool(x) is True
+    assertFalse(x)              bool(x) is False
+    assertIs(a, b)              a is b
+    assertIsNot(a, b)           a is not b
+    assertIsNone(x)             a is None
+    assertIsNotNone(x)          a is not None
+    assertIn(a, b)              a in b
+    assertNotIn(a, b)           a not in b
+    assertIsInstance(a, b)      isinstance(a, b)
+    assertNotIsInstance(a, b)   not isinstance(a, b)
+
+    assertAlmostEqual(a, b)     round(a-b, 7) == 0
+    assertNotAlmostEqual(a, b)  round(a-b, 7) != 0
+    assertGreater(a, b)         a > b
+    assertGreaterEqual(a, b)    a >= b
+    assertLess(a, b)            a < b
+    assertLessEqual(a, b)       a <= b
+    assertRegex(s, r)           r.search(s)
+    assertNotRegex(s, r)        not r.search(s)
+    assertCountEqual(a, b)      a and b have the same number of each
+                                element (regardless of order)
+
+    assertRaises(exc, fun, *args, **kwds)
+    assertRaisesRegex(exc, r, fun, *args, **kwds)
+    assertWarns(warn, fun, *args, **kwds)
+    assertWarnsRegex(warn, r, fun, *args, **kwds)
+    assertLogs(logger, level)
 """
+
+
 import unittest
-base = __import__('base').Base
+""" sys.path.append('../../models')
+"""
+from models.base import Base
 
 
 class TestBaseClass(unittest.TestCase):
     """ class containing testing functions for Base class
-        reference list of assert methods: 
-
-        assertEqual(a, b)           a == b
-        assertNotEqual(a, b)        a != b
-        assertTrue(x)               bool(x) is True
-        assertFalse(x)              bool(x) is False
-        assertIs(a, b)              a is b
-        assertIsNot(a, b)           a is not b
-        assertIsNone(x)             a is None
-        assertIsNotNone(x)          a is not None
-        assertIn(a, b)              a in b
-        assertNotIn(a, b)           a not in b
-        assertIsInstance(a, b)      isinstance(a, b)
-        assertNotIsInstance(a, b)   not isinstance(a, b)
-
-        assertAlmostEqual(a, b)     round(a-b, 7) == 0
-        assertNotAlmostEqual(a, b)  round(a-b, 7) != 0
-        assertGreater(a, b)         a > b
-        assertGreaterEqual(a, b)    a >= b
-        assertLess(a, b)            a < b
-        assertLessEqual(a, b)       a <= b
-        assertRegex(s, r)           r.search(s)
-        assertNotRegex(s, r)        not r.search(s)
-        assertCountEqual(a, b)      a and b have the same number of each
-                                    element (regardless of order)
-
-        assertRaises(exc, fun, *args, **kwds)
-        assertRaisesRegex(exc, r, fun, *args, **kwds)
-        assertWarns(warn, fun, *args, **kwds)
-        assertWarnsRegex(warn, r, fun, *args, **kwds)
-        assertLogs(logger, level)
     """
 
     def test_base_id(self):
-        instance = base()
+        instance = Base()
         self.assertIsNotNone(instance.id)
 
-    if __name__== '__main__':
-        unittest.main()
+if __name__== '__main__':
+    unittest.main()
