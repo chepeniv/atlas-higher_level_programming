f617df93f867d6e561b960aee12cab7702c9d1a5
chepeniv <jose.olmos@atlasschool.com>
2024-09-22 20:14:33 -0500

    [test] added id check for base

--- python-almost_a_circle/test/test_models/test_base.py
+++ python-almost_a_circle/test/test_models/test_base.py
@@ -42,26 +42,9 @@ class TestBaseClass(unittest.TestCase):
         assertLogs(logger, level)
     """
 
-    def test_max_at_end(self):
-        self.assertEqual(max_int([0, 1, 2]), 2)
-
-    def test_max_at_start(self):
-        self.assertEqual(max_int([2, 1, 0]), 2)
-
-    def test_max_at_middle(self):
-        self.assertEqual(max_int([2, 4, 1]), 4)
-
-    def test_one_negative(self):
-        self.assertEqual(max_int([1, 0, -1]), 1)
-
-    def test_only_negative(self):
-        self.assertEqual(max_int([-1, -6, -12]), -1)
-
-    def test_single_element(self):
-        self.assertEqual(max_int([3]), 3)
-
-    def test_empty(self):
-        self.assertEqual(max_int([]), None)
+    def test_base_id(self):
+        instance = base()
+        self.assertIsNotNone(instance.id)
 
     if __name__== '__main__':
         unittest.main()
