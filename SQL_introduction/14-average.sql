3bccb6265765b442d88f30ca0aa8547dac92538c
chepeniv <jose.olmos@atlasschool.com>
2024-10-12 14:47:34 -0500

    formatted output of statement

--- SQL_introduction/14-average.sql
+++ SQL_introduction/14-average.sql
@@ -1,4 +1,4 @@
 -- sql script that computes the average score of all records
 
 -- return the average of a field
-SELECT AVG(score) FROM second_table;
+SELECT AVG(score) AS average FROM second_table;
