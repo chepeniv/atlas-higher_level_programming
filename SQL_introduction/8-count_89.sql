e3d7fbc758716f1a621523310044977ff1a5da9f
chepeniv <jose.olmos@atlasschool.com>
2024-10-12 10:37:50 -0500

    new sql script counts the the number of records matching a given condition

---
+++ SQL_introduction/8-count_89.sql
@@ -0,0 +1,4 @@
+-- sql script that outputs the number of records matching a given condition
+
+-- count the number of records matching the condition
+SELECT COUNT(id) FROM first_table WHERE id = 89;
