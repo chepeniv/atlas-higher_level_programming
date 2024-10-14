292a5e6e7f80ac9af3c24ec44b4a2591ef61c2a4
chepeniv <jose.olmos@atlasschool.com>
2024-10-14 13:07:03 -0500

    switch order of create statements

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,10 +1,10 @@
 -- sql script that creates a new mysql database and user if they doesn't exist followed by explicit permissions
 
--- create new database
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-
 -- create new user
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- create new database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
 -- grant specific permissions
 GRANT SELECT ON hbt_0d_2.* TO 'user_0d_2'@'localhost';
