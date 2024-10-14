0e8b8c54263ffa4e7f5fb79ee1ed6030fca57d57
chepeniv <jose.olmos@atlasschool.com>
2024-10-14 12:57:47 -0500

    new sql script that creates user and grants all permissions

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- sql script that creates a new mysql user if it doesn't exist
+
+-- create new user
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
