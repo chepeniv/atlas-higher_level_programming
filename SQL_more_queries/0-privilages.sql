ff7bb368ee75b90a25de4896cbcd6afcd3dff8fb
chepeniv <jose.olmos@atlasschool.com>
2024-10-14 12:42:13 -0500

    created sql script that list all privaleges for given users

---
+++ SQL_more_queries/0-privilages.sql
@@ -0,0 +1,7 @@
+-- sql script that displays all permissions granted to specified users
+
+-- list permissions for given user
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- list permissions for given user
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
