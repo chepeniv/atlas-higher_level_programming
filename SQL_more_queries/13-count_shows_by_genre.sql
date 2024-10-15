d4f282dd2a14348ad5f7eb90157e0ac4c8d52cf0
chepeniv <jose.olmos@atlasschool.com>
2024-10-15 10:11:58 -0500

    added IS NOT NULL clause

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -4,6 +4,7 @@
 SELECT name AS genre, COUNT(name) AS number_of_shows
 FROM tv_genres LEFT JOIN tv_show_genres
 ON id = genre_id
+WHERE id IS NOT NULL
 GROUP BY name
 ORDER BY number_of_shows DESC;
 
