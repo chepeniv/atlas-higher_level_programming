5144d46a37e8c952dd0af75a581babd62575390f
chepeniv <jose.olmos@atlasschool.com>
2024-10-15 18:43:05 -0500

    py script extracting records that match a pattern

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" module demonstrating the use of mysql with python """
+
+import sys
+import MySQLdb
+
+
+def list_N_states(params):
+    """ function that accesses database and outputs entries """
+
+    usa_db = MySQLdb.connect(
+            user=params[1],
+            passwd=params[2],
+            database=params[3])
+
+    db_cursor = usa_db.cursor()
+    db_cursor.execute("SELECT * FROM states\
+            WHERE states.name LIKE 'n%'\
+            ORDER BY id")
+
+    for state in db_cursor.fetchall():
+        print("{}".format(state))
+
+
+if __name__ == "__main__":
+    list_N_states(sys.argv)
