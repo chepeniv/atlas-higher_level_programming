8eb86dec179ef0bdead355a4f3bb56bcbcc65b8d
chepeniv <jose.olmos@atlasschool.com>
2024-10-15 10:17:51 -0500

    changed to INNER JOIN instead of LEFT JOIN

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,10 +1,10 @@
 -- sql script that list all genres and the number of shows in each
 
 -- list genres and number of shows in each
+-- SELECT name , COUNT(name)
 SELECT name AS genre, COUNT(name) AS number_of_shows
-FROM tv_genres LEFT JOIN tv_show_genres
+FROM tv_genres JOIN tv_show_genres
 ON id = genre_id
-WHERE id IS NOT NULL
 GROUP BY name
 ORDER BY number_of_shows DESC;
 
