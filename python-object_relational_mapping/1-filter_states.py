5ed847867602c94cae7852b3a239852514d99e21
chepeniv <jose.olmos@atlasschool.com>
2024-10-15 18:04:06 -0500

    cleaned up a messed up file naming situation

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -5,19 +5,20 @@ import sys
 import MySQLdb
 
 
-def list_all_states(params):
+def list_matching_states(params):
     """ function that accesses database and outputs entries """
+
     usa_db = MySQLdb.connect(
             user=params[1],
             passwd=params[2],
             database=params[3])
+
     db_cursor = usa_db.cursor()
-    db_cursor.execute("SELECT * FROM states ORDER BY id")
-    usa_states = db_cursor.fetchall()
-    for state in usa_states:
+    db_cursor.execute("SELECT * FROM states WHERE states.name = %s ORDER BY id", (params[4],))
+
+    for state in db_cursor.fetchall():
         print("{}".format(state))
 
 
 if __name__ == "__main__":
-    params = sys.argv
-    list_all_states(params)
+    list_matching_states(sys.argv)
