c809c6ad5857bfddeeb830dcbb99a512c510c03b
chepeniv <jose.olmos@atlasschool.com>
2024-10-15 17:57:34 -0500

    py script that list all records matching a given string

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" module demonstrating the use of mysql with python """
+
+import sys
+import MySQLdb
+
+
+def list_all_states(params):
+    """ function that accesses database and outputs entries """
+    usa_db = MySQLdb.connect(
+            user=params[1],
+            passwd=params[2],
+            database=params[3])
+    db_cursor = usa_db.cursor()
+    db_cursor.execute("SELECT * FROM states ORDER BY id")
+    usa_states = db_cursor.fetchall()
+    for state in usa_states:
+        print("{}".format(state))
+
+
+if __name__ == "__main__":
+    params = sys.argv
+    list_all_states(params)
