5c9a638e28bda307e2f447eed9f17c8b61ef538b
chepeniv <jose.olmos@atlasschool.com>
2024-10-15 11:00:25 -0500

    sql script that list all genres of a given show

---
+++ SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,10 @@
+-- sql script that list all genres of a given show
+
+-- list genres of a given show
+SELECT tv_genres.name
+FROM tv_shows 
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY tv_genres.name;
+
