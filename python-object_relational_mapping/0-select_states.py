80b7563f0fcbda600997ffb7be25e766459d51fd
chepeniv <jose.olmos@atlasschool.com>
2024-10-15 17:17:07 -0500

    added doc-comments

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+""" module demonstrating the use of mysql with python """
 
 import sys
 import MySQLdb
 
 def list_all_states(params):
+    """ function that accesses database and outputs entries """
     usa_db = MySQLdb.connect(user=params[1], passwd=params[2], database=params[3])
     db_cursor = usa_db.cursor()
     db_cursor.execute("SELECT * FROM states ORDER BY id")
