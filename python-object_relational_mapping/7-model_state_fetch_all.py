0d1ef0043abedd209677c0a6ee29cc88d1db35bc
chepeniv <jose.olmos@atlasschool.com>
2024-10-17 16:02:52 -0500

    added query_all_states() function

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,21 +1,38 @@
 #!/usr/bin/python3
-""" module setting up a Base class and Derived class for mysql
-""" 
+"""
+connect to database and query it
+"""
 
-from sqlalchemy.ext.declarative import declarative_base
-from model_states import State, Base
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import sessionmaker
+import sys
 
-def query(params)
-    """ definition of class that links to mysql table """ 
-    __tablename__ = 'states'
-    identity = Column(
-            "id",
-            Integer(11),
-            primary_key=True,
-            autoincrement=True,
-            unique=True,
-            nullable=False)
-    name = Column(String(128), nullable=False)
+
+def query_all_states(argument_list):
+    """
+    returns all records from states table from given database
+    """
+
+    username = argument_list[1]
+    password = argument_list[2]
+    database = argument_list[3]
+
+    db_url = "mysql+mysqldb://{}:{}@localhost/{}".format(
+            username,
+            password,
+            database)
+
+    db_engine = create_engine(db_url, pool_pre_ping=True)
+    Base.metadata.create_all(db_engine)
+    Session = sessionmaker(db_engine)
+    current_session = Session()
+
+    all_states = current_session.query(State)
+
+    for state in all_states:
+        print("{}: {}".format(state.id, state.name))
+
 
 if __name__ == "__main__":
-    query(sys.argv)
+    query_all_states(sys.argv)
