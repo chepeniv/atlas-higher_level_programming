8875fd6107a6c3112f357ecb26ba38b3e304dd7e
chepeniv <jose.olmos@atlasschool.com>
2024-10-17 15:18:57 -0500

    changed how declarative_base is imported

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -3,7 +3,7 @@
 """ 
 
 from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 import sys
 
 
