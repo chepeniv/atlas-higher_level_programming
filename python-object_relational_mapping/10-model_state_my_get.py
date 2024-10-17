7a9f48f28c98c6d90b3fd19c4466f9605eecc539
chepeniv <jose.olmos@atlasschool.com>
2024-10-17 17:54:56 -0500

    created function that finds and prints state record based on name given

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+connect to database and query it
+"""
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+
+def query_state_name(argument_list):
+    """
+    returns all records that match the given filter
+    from states table from given database
+    """
+
+    username = argument_list[1]
+    password = argument_list[2]
+    database = argument_list[3]
+    statename = argument_list[4]
+
+    db_url = "mysql+mysqldb://{}:{}@localhost/{}".format(
+            username,
+            password,
+            database)
+
+    db_engine = create_engine(db_url, pool_pre_ping=True)
+    Base.metadata.create_all(db_engine)
+    Session = sessionmaker(db_engine)
+    current_session = Session()
+
+    found_state = current_session.query(State).filter(State.name == statename).one()
+
+    if found_state is not None:
+        print("{}".format(found_state.id))
+    else:
+        print("Not Found")
+
+
+if __name__ == "__main__":
+    query_state_name(sys.argv)
