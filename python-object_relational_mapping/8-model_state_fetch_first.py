99333ee06d8c47f1e7dedc888a066dd2c5cac924
chepeniv <jose.olmos@atlasschool.com>
2024-10-17 16:15:29 -0500

    created function that quiries for item with id = 1

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+connect to database and query it
+"""
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+
+def query_first_state(argument_list):
+    """
+    returns all records that match the given filter
+    from states table from given database
+    """
+
+    username = argument_list[1]
+    password = argument_list[2]
+    database = argument_list[3]
+
+    db_url = "mysql+mysqldb://{}:{}@localhost/{}".format(
+            username,
+            password,
+            database)
+
+    db_engine = create_engine(db_url, pool_pre_ping=True)
+    Base.metadata.create_all(db_engine)
+    Session = sessionmaker(db_engine)
+    current_session = Session()
+
+    all_states = current_session.query(State).filter(State.id == 1)
+
+    for state in all_states:
+        print("{}: {}".format(state.id, state.name))
+
+
+if __name__ == "__main__":
+    query_first_state(sys.argv)
