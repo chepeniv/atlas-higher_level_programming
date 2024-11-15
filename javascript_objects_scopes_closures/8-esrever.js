78812a9e8c83e2b0443f90c44590dafba962fd4a
chepeniv <jose.olmos@atlasschool.com>
2024-11-14 19:16:57 -0600

    didn't read the instructions carefully, removed use of .reverse()

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  return list.reverse();
+  let reversed = [];
+  for (let i = 0; i < list.length; i++) {
+    reversed.push(list[list.length - 1 - i]);
+  }
+  return reversed;
 };
