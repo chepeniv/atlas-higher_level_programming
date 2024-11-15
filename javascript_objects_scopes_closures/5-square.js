256394abcf5f72e2ca7e9efc86e34131c6753670
chepeniv <jose.olmos@atlasschool.com>
2024-11-14 18:17:59 -0600

    removed redefinitions carried over from duplication

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -6,25 +6,6 @@ class Square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
-
-  print () {
-    let xline = 'X';
-    xline = xline.repeat(this.width);
-    for (let i = 0; i < this.height; i++) {
-      console.log(xline);
-    }
-  }
-
-  rotate () {
-    const hold = this.width;
-    this.width = this.height;
-    this.height = hold;
-  }
-
-  double () {
-    this.width *= 2;
-    this.height *= 2;
-  }
 }
 
 module.exports = Square;
