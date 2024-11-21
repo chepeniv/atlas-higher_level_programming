006b28810684c3b3e5ab0abb09f036623aa6d174
chepeniv <jose.olmos@atlasschool.com>
2024-11-21 10:35:14 -0600

    completed script that read and prints file contents; removed working comments

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,20 +1,9 @@
 #!/usr/bin/node
 
-// read and print the contents of a file
-// do so in utf-8 mode
-// if an error occurs during reading print the error object
-// usage: ./0-readme.js filename.ext
 const fs = require('node:fs');
-const buffer = require('node:buffer');
-
 const filename = process.argv[2];
-const readBuffer = buffer.Buffer.alloc(512);
 
-try {
-  const fd = fs.openSync(filename, 'r');
-  fs.readSync(fd, readBuffer);
-  const fileString = readBuffer.toString();
-  console.log(fileString);
-} catch (error) {
-  console.log(error);
-}
+fs.readFile(filename, 'utf8', (err, data) => {
+  if (err) console.log(err);
+  else console.log(data);
+});
