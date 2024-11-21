97213a6b27cffba8a762f860632d819874cd2c94
chepeniv <jose.olmos@atlasschool.com>
2024-11-21 15:47:30 -0600

    (init) script that extracts json data and processes it

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+
+let tasks;
+let summary = {};
+let completedTasks = [];
+
+request(url, (err, response, body) => {
+  if (err) console.log(err);
+  else {
+	// turn webdata to json
+	tasks = JSON.parse(body)
+	// extract only completed task
+	completedTasks = tasks.filter((item) => item.completed)
+	// iterate through each user id
+	for (let i = 0; i < 11; i++) {
+		// get current user's completed tasks
+		let count = completedTasks.filter((item) => item.userId === i)
+		// count the completed tasks and if not zero
+		// add them to the summary
+		count = count.length;
+		if (count != 0) summary[i] = count;
+	}
+	console.log(summary);
+  }
+});
