30ae504dd67f1fa2555248be011d4476e2d8a454
chepeniv <jose.olmos@atlasschool.com>
2024-11-22 17:47:46 -0600

    created js that selects and updates css using jquery

---
+++ javascript-web_jquery/1-script.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+// update the color of <header> content to #ff0000
+// use the jquery api
+// cannot use document.querySelector 
+const header = $('header');
+header.css('color', '#ff0000');
