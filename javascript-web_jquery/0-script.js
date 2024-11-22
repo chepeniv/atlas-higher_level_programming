3b0ef93b822d722864619c2ecde8efb7214b54bd
chepeniv <jose.olmos@atlasschool.com>
2024-11-22 17:47:10 -0600

    added working comments back

--- javascript-web_jquery/0-script.js
+++ javascript-web_jquery/0-script.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
 
+// select and update the <header> tag's color to #ff0000
+// use document.querySelector
+// cannot use the jquery api
 const header = document.querySelector('header');
 header.style.color = '#ff0000';
