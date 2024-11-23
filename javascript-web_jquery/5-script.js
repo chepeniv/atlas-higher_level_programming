25cff2cf1ef21c52698460019edb84230c7144d2
chepeniv <jose.olmos@atlasschool.com>
2024-11-22 19:26:39 -0600

    created js that appends <li> elemnents to <lu> upon click

---
+++ javascript-web_jquery/5-script.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+// add <li>Item</li> to UL.my_list whenever div#add_item is clicked
+// cannot use document.querySelecto 
+// use the jquery api
+$('div#add_item').click(() => {
+  $('<li>Item</li>').appendTo('ul.my_list');
+});
