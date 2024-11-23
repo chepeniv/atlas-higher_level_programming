932ef8be3a4485aa480f7b755b2332dce42f7527
chepeniv <jose.olmos@atlasschool.com>
2024-11-22 18:13:08 -0600

    completed js that modifies one element when another is clicked using jquery

---
+++ javascript-web_jquery/2-script.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+// update the color of <header> content to #ff0000 when
+// div#red_header is clicked
+// cannot use document.querySelector 
+// use the jquery api
+$('div#red_header').click(() => {
+  $('header').css('color', '#ff0000');
+});
---
+++ javascript-web_jquery/3-script.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+// add the class red to <header> content to #ff0000 when
+// div#red_header is clicked
+// cannot use document.querySelector 
+// use the jquery api
+$('div#red_header').click(() => {
+  $('header').addClass('red');
+});
