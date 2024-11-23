d554bdf1cd70e62de5828741977cb9b0a25afdd4
chepeniv <jose.olmos@atlasschool.com>
2024-11-22 19:33:45 -0600

    wrote js that updates the content of the header upon click event

---
+++ javascript-web_jquery/6-script.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+// update the content of <header> to 'New Header!!!' 
+// whenever div#update_header is clicked
+// cannot use document.querySelecto 
+// must use the jquery api
+$('div#update_header').click(() => {
+  $('header').text('New Header!!!');
+});
